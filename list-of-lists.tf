b9a08ec12603b3f9eb9c38bad1c47707a2101417
Jacob Luszcz <jacob.luszcz@gmail.com>
2022-01-24 07:35:12 -0500

    Increase minimum TLS version supported

--- list-of-lists.tf
+++ list-of-lists.tf
@@ -162,7 +162,7 @@ resource "aws_cloudfront_distribution" "site" {
 
   viewer_certificate {
     acm_certificate_arn      = aws_acm_certificate.cert.arn
-    minimum_protocol_version = "TLSv1"
+    minimum_protocol_version = "TLSv1.2_2021"
     ssl_support_method       = "sni-only"
   }
 }
