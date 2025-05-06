6b995d89d960c98634ebbcd079fcfb68d3bce976
Jacob Luszcz <jacob.luszcz@gmail.com>
2025-05-06 08:09:14 -0400

    Switch aws_iam_openid_connect_provider from resource to data

--- list-of-lists.tf
+++ list-of-lists.tf
@@ -355,12 +355,8 @@ resource "aws_lambda_function" "lambda" {
   }
 }
 
-resource "aws_iam_openid_connect_provider" "github" {
+data "aws_iam_openid_connect_provider" "github" {
   url = "https://token.actions.githubusercontent.com"
-
-  client_id_list = ["sts.amazonaws.com"]
-
-  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
 }
 
 data "aws_iam_policy_document" "github_update" {
@@ -384,7 +380,7 @@ resource "aws_iam_role" "github_update" {
       {
         Effect = "Allow",
         Principal = {
-          Federated = aws_iam_openid_connect_provider.github.arn
+          Federated = "${data.aws_iam_openid_connect_provider.github.arn}"
         },
         Action = "sts:AssumeRoleWithWebIdentity",
         Condition = {
