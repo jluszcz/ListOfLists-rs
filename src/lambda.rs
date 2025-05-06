fc7651e48f549131498597d895be67e9ff825498
Jacob Luszcz <jacob.luszcz@gmail.com>
2025-05-06 09:00:07 -0400

    Log information about triggering S3 events

--- src/lambda.rs
+++ src/lambda.rs
@@ -1,5 +1,7 @@
+use aws_lambda_events::s3::S3Event;
 use lambda_runtime::{LambdaEvent, service_fn};
 use list_of_lists::generator;
+use log::info;
 use serde_json::{Value, json};
 use std::env;
 use std::error::Error;
@@ -16,11 +18,24 @@ async fn main() -> Result<(), LambdaError> {
     Ok(())
 }
 
-async fn function(_: LambdaEvent<Value>) -> Result<Value, LambdaError> {
+async fn function(event: LambdaEvent<Value>) -> Result<Value, LambdaError> {
     list_of_lists::set_up_logger(module_path!(), false)?;
 
     let site_name = env::var(list_of_lists::SITE_NAME_VAR)?;
     let site_url = env::var(list_of_lists::SITE_URL_VAR)?;
+
+    let event: S3Event = serde_json::from_value(event.payload)?;
+    if event.records.len() == 1 {
+        for record in event.records {
+            let bucket = record.s3.bucket.name;
+            let key = record.s3.object.key;
+            if let (Some(bucket), Some(key)) = (bucket, key) {
+                info!("Updating {site_name} on update of {bucket}/{key}");
+            }
+        }
+    } else {
+        info!("Updating {site_name} on multiple updates");
+    }
 
     generator::update_site(site_name, site_url, USE_S3, MINIFY).await?;
 
