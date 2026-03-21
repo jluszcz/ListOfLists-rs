6a57842a0f62883106d38ec591b6d1c3fa0fb724
Jacob Luszcz <jacob.luszcz@gmail.com>
2026-03-21 13:28:07 -0400

    Consolidate index.template upload into a single workflow (#83)
    Replace three per-site update-index workflows with one shared workflow
    using a new list-of-lists.github-update IAM role that uploads directly
    to the generator bucket.

--- list-of-lists.tf
+++ list-of-lists.tf
@@ -419,6 +419,48 @@ resource "aws_iam_role_policy_attachment" "github_update" {
   policy_arn = aws_iam_policy.github_update.arn
 }
 
+data "aws_iam_policy_document" "github_update_template" {
+  statement {
+    actions   = ["s3:PutObject"]
+    resources = ["${aws_s3_bucket.generator.arn}/index.template"]
+  }
+}
+
+resource "aws_iam_policy" "github_update_template" {
+  name   = "list-of-lists.github-update"
+  policy = data.aws_iam_policy_document.github_update_template.json
+}
+
+resource "aws_iam_role" "github_update_template" {
+  name = "list-of-lists.github-update"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Federated = data.aws_iam_openid_connect_provider.github.arn
+        },
+        Action = "sts:AssumeRoleWithWebIdentity",
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" : "sts.amazonaws.com"
+          }
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" : "repo:jluszcz/ListOfLists-rs:*"
+          },
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "github_update_template" {
+  role       = aws_iam_role.github_update_template.name
+  policy_arn = aws_iam_policy.github_update_template.arn
+}
+
 data "aws_iam_policy_document" "github_deploy" {
   statement {
     actions   = ["s3:PutObject"]
